f9d42bf7f237c7a51aa8b566c088af8691648fe3
Matheus Alexandre <111097490+R4dars@users.noreply.github.com>
2023-11-22 23:53:28 -0300

    atualização do UsuarioController

--- UsuarioController.java
+++ UsuarioController.java
@@ -15,4 +15,31 @@ public class UsuarioController {
         Usuario novoUsuario = usuarioService.cadastrarUsuario(usuario);
         return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obterUsuario(@PathVariable Long id) {
+        Usuario usuario = UsuarioService.obterUsuario(id);
+        if (usuario != null) {
+            return new ResponseEntity<>(Usuario, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }   
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarioService.atualizarUsuario(id, usuario);
+        if (usuarioAtualizado != null) {
+            return new ResponseEntity<>(usuarioAtualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
